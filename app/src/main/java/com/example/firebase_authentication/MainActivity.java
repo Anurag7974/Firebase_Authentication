fcfd86977a9a446bc3a5089ba1c4dd2d4e204349
Anurag <anurag@git79.com>
2022-12-10 01:24:47 +0530

    completed code MainActivity code side.

--- app/src/main/java/com/example/firebase_authentication/MainActivity.java
+++ app/src/main/java/com/example/firebase_authentication/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.firebase_authentication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.firebase_authentication.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
+    FirebaseFirestore firebaseFirestore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        // Click the signUp button so you jump the login page.
+        binding.signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = binding.editName.getText().toString();
+                String number = binding.editMobile.getText().toString();
+                String email = binding.editEmail.getText().toString().trim(); // trim() it's used the no space are there.
+                String password = binding.editPassword.getText().toString();
+
+                progressDialog.show();
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                startActivity(new Intent(MainActivity.this, SignUp_Activity.class));
+                                progressDialog.cancel();
+
+                                firebaseFirestore.collection("User")
+                                        .document(FirebaseAuth.getInstance().getUid())
+                                        .set(new UserModel(name, number, email));
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                progressDialog.cancel();
+                            }
+                        });
+            }
+        });
     }
 }
+
+
